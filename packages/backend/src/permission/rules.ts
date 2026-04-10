9c1023c4ccb25aa4095f15b83d8c0c2f45a67a64
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2026-04-10 11:13:45 +0900

    feat: rules 추가

---
+++ packages/backend/src/permission/rules.ts
@@ -0,0 +1,13 @@
+import {PolicyQueryUser} from '@backstage/plugin-permission-node';
+import isAdmin from "../utils/isAdmin";
+import isTeamLeader from "../utils/isTeamLeader";
+import isLgePartners from "../utils/isLgePartners";
+
+
+export type PermissionRule = (user: PolicyQueryUser) => boolean;
+
+export const PERMISSION_RULES: Record<string, PermissionRule> = {
+    'admin-panel.view': (user) => isAdmin(user),
+    'catalog.entity.delete': (user) => !!(isAdmin(user) || isTeamLeader(user)),
+    'scaffolder.task.create': (user) => !isLgePartners(user),
+};
